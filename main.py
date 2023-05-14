ad466e77abfe8990ff14539f446bc76c096b3043
sml-msn <121129280+sml-msn@users.noreply.github.com>
2023-05-14 23:05:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,57 +33,57 @@ def load_image():
 
 
 def getContent_1(thingNameLst):
-  thingName = ''
-  firstWord = True
-  for word in thingNameLst:
-    if firstWord:
-      thingName = thingName + word
-      firstWord = False
-    else:
-      thingName = thingName + '+' + word
+    thingName = ''
+    firstWord = True
+    for word in thingNameLst:
+        if firstWord:
+            thingName = thingName + word
+            firstWord = False
+        else:
+            thingName = thingName + '+' + word
   
-  url = f'https://diyprojects.com/?s={thingName}'
-  headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-  page = requests.get(url, headers=headers, verify=False)
+    url = f'https://diyprojects.com/?s={thingName}'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    page = requests.get(url, headers=headers, verify=False)
   
-  filteredData = []
-  allData = []
+    filteredData = []
+    allData = []
   
-  soup = BeautifulSoup(page.text, "html.parser")
+    soup = BeautifulSoup(page.text, "html.parser")
   
-  allData = soup.findAll('article')
-  for data in allData:
-    filtered = []
-    if data.find('a', attrs={'class': 'entry-title-link'}) is not None:
-      filtered.append(data.find('a', attrs={'class': 'entry-title-link'}).get('href'))
-      filtered.append(data.find('a', attrs={'class': 'entry-title-link'}).text)
-      filtered.append(data.find('img', attrs={'class': 'alignleft post-image entry-image'}).get('src'))
-      filtered.append(data.find('div', attrs={'class': 'entry-content'}).find('p').text)
-      filteredData.append(filtered)
+    allData = soup.findAll('article')
+    for data in allData:
+        filtered = []
+        if data.find('a', attrs={'class': 'entry-title-link'}) is not None:
+            filtered.append(data.find('a', attrs={'class': 'entry-title-link'}).get('href'))
+            filtered.append(data.find('a', attrs={'class': 'entry-title-link'}).text)
+            filtered.append(data.find('img', attrs={'class': 'alignleft post-image entry-image'}).get('src'))
+            filtered.append(data.find('div', attrs={'class': 'entry-content'}).find('p').text)
+            filteredData.append(filtered)
   
-  return filteredData
+    return filteredData
 
 def printDiyData(thingNameLst):
-  diyData = getContent_1(thingNameLst)
-  if len(diyData) < 10:
-    x = len(diyData)
-  else:
-    x = 10
-  for i in range(x):
-    st.header(diyData[i][1])
-    st.write(diyData[i][0])
-    st.image(diyData[i][2])
-    if diyData[i][3] != None:
-      st.write(diyData[i][3])
-    st.write('------------------')
+    diyData = getContent_1(thingNameLst)
+    if len(diyData) < 10:
+        x = len(diyData)
+    else:
+        x = 10
+    for i in range(x):
+        st.header(diyData[i][1])
+        st.write(diyData[i][0])
+        st.image(diyData[i][2])
+        if diyData[i][3] != None:
+            st.write(diyData[i][3])
+        st.write('------------------')
 
 def predict(img, model, candidateId):
-  x = preprocess_image(img)
-  preds = model.predict(x)
-  classes = decode_predictions(preds, top=3)[0]
-  st.subheader(f'it is a {classes[candidateId][1]}')
-  thingNameLst = classes[candidateId][1].split('_')
-  printDiyData(thingNameLst)
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    classes = decode_predictions(preds, top=3)[0]
+    st.subheader(f'it is a {classes[candidateId][1]}')
+    thingNameLst = classes[candidateId][1].split('_')
+    printDiyData(thingNameLst)
   
 
 model = load_model()
@@ -93,6 +93,5 @@ st.title('Give it a chance!')
 img = load_image()
 result = st.button('Scan image')
 if result:
-  candidateId = 0
-  predict(img, model, candidateId)
-
+    candidateId = 0
+    predict(img, model, candidateId)
