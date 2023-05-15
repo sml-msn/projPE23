fece06d324fb1e8769f5bbd57bf28cd6698fc39a
Jul-Kras <122283444+Jul-Kras@users.noreply.github.com>
2023-05-15 15:20:22 +0500

    Update test_main.py
    Add new function test_url_accessibility_2

--- test_main.py
+++ test_main.py
@@ -9,3 +9,11 @@ def test_url_accessibility():
     page = requests.get(url, headers=headers, verify=False)
     status = page.status_code
     assert status == 200
+
+# check url accessibility
+def test_url_accessibility_2():
+    url = 'https://www.hometalk.com'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    page = requests.get(url, headers=headers, verify=False)
+    status = page.status_code
+    assert status == 200    
