d83ed7ca6ff1dd04c129bcf9e5de5e9babf4f5b2
sml-msn <121129280+sml-msn@users.noreply.github.com>
2023-06-15 18:06:49 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,27 @@
 import streamlit as st
 import pytest
 import requests
+from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from main import preprocess_image, load_image, print_predictions
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return EfficientNetB0(weights='imagenet')
+
+def test_prep_img():
+    #with open() as image_data:
+    #assert preprocess_image(Image.open(io.BytesIO(image_data))) is not None
+    assert preprocess_image(Image.open(r"pictures/wine_bottle.jpg")) is not None
+
+def test_jpg():
+    x = preprocess_image(Image.open(r"pictures/wine_bottle.jpg"))
+    preds = model.predict(x)
+    classes = decode_predictions(preds, top=3)[0] # takes 3 preds and puts in predLst
+    assert classes[0][1].split('_').count('bottle') > 0
 
 # check url accessibility
 def test_url_accessibility():
